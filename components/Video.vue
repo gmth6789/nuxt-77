7e6380b73156ff12b579f91b629e4c3d3c118cd9
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-08-28 09:41:36 +0700

    1 file modified

---
+++ components/Video.vue
@@ -0,0 +1,23 @@
+<template>
+  <nut-video :source="source" :options="options"></nut-video>
+</template>
+<script lang="ts">
+  import { toRefs, reactive } from 'vue';
+  export default {
+    setup() {
+      const state = reactive({
+        source: {
+          src: 'src/static/video/F-fin.mp4',
+          type: 'video/mp4'
+        },
+        options: {
+          autoplay: true,
+          muted: false,
+          controls: true
+        }
+      });
+
+      return { ...toRefs(state) };
+    }
+  };
+</script>
